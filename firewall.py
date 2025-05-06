8c211ea5c14ef0e2225f3eab87101800ee5adb12
LukasGlahn <65539328+LukasGlahn@users.noreply.github.com>
2025-05-06 16:19:28 +0200

    fix so firewall checker can give resoults to main script and handel ipv6

--- firewall.py
+++ firewall.py
@@ -3,6 +3,16 @@ import subprocess
 
 class FireWallChecker():
     def __init__(self):
+        self.ipv4 = self.run_command(['sudo','iptables','-S'])
+        self.ipv6 = self.run_command(['sudo','ip6tables','-S'])
+        
+        self.rules6 = """
+-P INPUT ACCEPT
+-P FORWARD ACCEPT
+-P OUTPUT ACCEPT
+-N DOCKER-USER
+"""
+        
         self.rules = """
 -P INPUT DROP
 -P FORWARD DROP
@@ -14,7 +24,7 @@ class FireWallChecker():
 -N DOCKER-ISOLATION-STAGE-1
 -N DOCKER-ISOLATION-STAGE-2
 -N DOCKER-USER
--A INPUT -rule lo -j ACCEPT
+-A INPUT -i lo -j ACCEPT
 -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 -A INPUT -p udp -m udp --dport 5353 -j ACCEPT
 -A INPUT -m pkttype --pkt-type multicast -j ACCEPT
@@ -24,8 +34,8 @@ class FireWallChecker():
 -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
 -A FORWARD -j DOCKER-USER
 -A FORWARD -j DOCKER-FORWARD
--A DOCKER ! -rule br-63af15588383 -o br-63af15588383 -j DROP
--A DOCKER ! -rule docker0 -o docker0 -j DROP
+-A DOCKER ! -i br-63af15588383 -o br-63af15588383 -j DROP
+-A DOCKER ! -i docker0 -o docker0 -j DROP
 -A DOCKER-BRIDGE -o br-63af15588383 -j DOCKER
 -A DOCKER-BRIDGE -o docker0 -j DOCKER
 -A DOCKER-CT -o br-63af15588383 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
@@ -33,10 +43,10 @@ class FireWallChecker():
 -A DOCKER-FORWARD -j DOCKER-CT
 -A DOCKER-FORWARD -j DOCKER-ISOLATION-STAGE-1
 -A DOCKER-FORWARD -j DOCKER-BRIDGE
--A DOCKER-FORWARD -rule br-63af15588383 -j ACCEPT
--A DOCKER-FORWARD -rule docker0 -j ACCEPT
--A DOCKER-ISOLATION-STAGE-1 -rule br-63af15588383 ! -o br-63af15588383 -j DOCKER-ISOLATION-STAGE-2
--A DOCKER-ISOLATION-STAGE-1 -rule docker0 ! -o docker0 -j DOCKER-ISOLATION-STAGE-2
+-A DOCKER-FORWARD -i br-63af15588383 -j ACCEPT
+-A DOCKER-FORWARD -i docker0 -j ACCEPT
+-A DOCKER-ISOLATION-STAGE-1 -i br-63af15588383 ! -o br-63af15588383 -j DOCKER-ISOLATION-STAGE-2
+-A DOCKER-ISOLATION-STAGE-1 -i docker0 ! -o docker0 -j DOCKER-ISOLATION-STAGE-2
 -A DOCKER-ISOLATION-STAGE-2 -o docker0 -j DROP
 -A DOCKER-ISOLATION-STAGE-2 -o br-63af15588383 -j DROP
 -A DOCKER-USER -j RETURN
@@ -64,51 +74,13 @@ class FireWallChecker():
         else:
             return num2
 
-    def check_difrense(self):
-        default_rules = self.rules.split("\n")
+    def check_difrense(self, default_rules, system_rules):
+        
+        vialations = []
+        
+        default_rules = default_rules.split("\n")
         default_rules = self.remove_values_from_list(default_rules,"")
-
-        """system_rules = self.run_command(['sudo','iptables','-S'])
-        system_rules = system_rules.split("\n")
-        system_rules = self.remove_values_from_list(system_rules,"")"""
-        system_rules ="""
--P INPUT DROP
--P FORWARD DROP
--P OUTPUT ACCEPT
--N DOCKER
--N DOCKER-BRIDGE
--N DOCKER-CT
--N DOCKER-FORWARD
--N DOCKER-ISOLATION-STAGE-1
--N DOCKER-ISOLATION-STAGE-2
--N DOCKER-USER
--A INPUT -rule lo -j ACCEPT
--A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
--A INPUT -p udp -m udp --dport 5353 -j ACCEPT
--A INPUT -m pkttype --pkt-type multicast -j ACCEPT
--A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
--A INPUT -p tcp -m tcp --dport 868 -j ACCEPT
--A INPUT -s 172.0.0.0/8 -p tcp -m tcp --dport 50051 -j ACCEPT
--A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
--A FORWARD -j DOCKER-USER
--A FORWARD -j DOCKER-FORWARD
--A DOCKER ! -rule br-63af15588383 -o br-63af15588383 -j DROP
--A DOCKER ! -rule docker0 -o docker0 -j DROP
--A DOCKER-BRIDGE -o br-63af15588383 -j DOCKER
--A DOCKER-BRIDGE -o docker0 -j DOCKER
--A DOCKER-CT -o br-63af15588383 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
--A DOCKER-CT -o docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
--A DOCKER-FORWARD -j DOCKER-CT
--A DOCKER-FORWARD -j DOCKER-ISOLATION-STAGE-1
--A DOCKER-FORWARD -j DOCKER-BRIDGE
--A DOCKER-FORWARD -rule br-63af15588383 -j ACCEPT
--A DOCKER-FORWARD -rule docker0 -j ACCEPT
--A DOCKER-ISOLATION-STAGE-1 -rule br-63af15588383 ! -o br-63af15588383 -j DOCKER-ISOLATION-STAGE-2
--A DOCKER-ISOLATION-STAGE-1 -rule docker0 ! -o docker0 -j DOCKER-ISOLATION-STAGE-2
--A DOCKER-ISOLATION-STAGE-2 -o docker0 -j DROP
--A DOCKER-ISOLATION-STAGE-2 -o br-63af15588383 -j DROP
--A DOCKER-USER -j RETURN
-"""
+        
         system_rules = system_rules.split("\n")
         system_rules = self.remove_values_from_list(system_rules,"")
         
@@ -124,6 +96,21 @@ class FireWallChecker():
             missing_rules = list(set(default_rules)-set(system_rules))
             print(missing_rules)
             print(new_rules)
+            
+            if len(missing_rules) > 0:
+                vialations.append( {
+                    "problem": "firewall",
+                    "info": f"Missing ruels {missing_rules} was found",
+                    "severity" : 3
+                    })
+            
+            if len(new_rules) > 0:
+                vialations.append( {
+                    "problem": "firewall",
+                    "info": f"New ruels {new_rules} was found",
+                    "severity" : 3
+                    })
+            
             for rule in range(len(system_rules)):
                 #rules that are out of index of the default_rules
                 if rule >= len(system_rules) or rule >= len(default_rules):
@@ -133,6 +120,11 @@ class FireWallChecker():
                         if abs(moved_places) > 0:
                             print(list_off_by)
                             print(f"Rule '{system_rules[rule]}' moved by {moved_places} position")
+                            vialations.append( {
+                                "problem": "firewall",
+                                "info": f"Rule '{system_rules[rule]}' moved by {moved_places} position",
+                                "severity" : 3
+                                })
                             list_off_by += 1
 
                 # check if rule is the same as one in the default_rules and in the same place
@@ -145,10 +137,24 @@ class FireWallChecker():
                     moved_places = expected_index - rule + list_off_by
                     if default_rules[rule] in missing_rules:
                         print("it be gone",system_rules[rule])
+                        
+                        vialations.append({
+                                "problem": "firewall",
+                                "info": f"Rule '{system_rules[rule]}' is missing",
+                                "severity" : 3
+                                })
+                        
                         list_off_by -= 1
                     elif abs(moved_places) > 0:
                         print(list_off_by)
                         print(f"Rule '{system_rules[rule]}' moved by {moved_places} position")
+                        
+                        vialations.append( {
+                                "problem": "firewall",
+                                "info": f"Rule '{system_rules[rule]}' moved by {moved_places} position",
+                                "severity" : 3
+                                })
+                        
                         list_off_by += 1
                     
 
@@ -161,9 +167,19 @@ class FireWallChecker():
         #get get all changes relating to a roule being removed
         if len(system_rules) < len(default_rules):
             print("idk")
+        
+        return vialations
+            
+    def check_system_rules(self):
+        vialations = []
+        
+        vialations += self.check_difrense(self.rules, self.ipv4)
+        vialations += self.check_difrense(self.rules6, self.ipv6)
+        
+        return vialations
 
 
 if __name__ == "__main__":
     firewalchecker = FireWallChecker()
-    firewalchecker.check_difrense()
+    print(firewalchecker.check_system_rules())
 
